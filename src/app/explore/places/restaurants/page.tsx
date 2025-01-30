29105b08fd9ca5775a13311bad76b13b559990b0
Jiwoo <erase0250@gmail.com>
2025-01-30 14:50:50 +0900

    Design : 식당 및 숙소 리스트 페이지 UI
    - src/app/explore/places/page.tsx : 식당/숙소 카테고리 눌렀을 때 해당 페이지로 이동하게 수정
    
    - src/app/explore/places/restaurants/page.tsx : 식당 리스트 페이지
    
    - src/app/explore/places/accommodations/page.tsx : 숙소 리스트 페이지
    
    - src/components/places/... : 식당/숙소 카드 컴포넌트 추가
    
    - 그 외 이미지 추가

---
+++ src/app/explore/places/restaurants/page.tsx
@@ -0,0 +1,177 @@
+// 식당 페이지
+
+"use client";
+
+import Header from "@/components/common/Header";
+import Footer from "@/components/common/Footer";
+import Image from "next/image";
+import { useState } from "react";
+import Link from "next/link";
+import RestaurantCard from "@/components/places/restaurantCard";
+
+export default function Restaurants() {
+   const [selectedCategory, setSelectedCategory] = useState("식당");
+   const [searchTerm, setSearchTerm] = useState("");
+
+   const categories = [
+      { name: "식당", link: "/places/restaurants" },
+      { name: "숙소", link: "/places/accommodations" },
+   ];
+
+   // 입력된 검색어가 변경될 때 처리
+   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearchTerm(e.target.value);
+   };
+
+   // 검색 버튼 클릭 시 처리
+   const handleSearchClick = () => {
+      // 실제 API 요청이나 필터링 로직을 넣을 부분
+      console.log("카테고리:", selectedCategory, "검색어:", searchTerm);
+   };
+
+   return (
+      <div className="min-h-screen">
+         <Header />
+
+         <div className="relative mb-[72px]">
+            <Image
+               width={0}
+               height={0}
+               sizes="100vw"
+               src="/images/places/banner.png"
+               alt="banner"
+               className="w-full h-[392px] object-cover"
+            />
+            {/* 배너 텍스트 */}
+            <div className="absolute top-[30%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
+               <h2 className="text-4xl font-bold">강원도의 맛집과 숙박 정보</h2>
+            </div>
+
+            {/* 검색창 */}
+            <div className="absolute inset-x-0 top-[60%] mx-auto w-[700px] p-7 shadow-xl bg-white rounded-lg z-20 transform -translate-y-1/2">
+               <div className="flex justify-between">
+                  {/* 식당&숙소 카테고리 */}
+                  <ul className="flex gap-3 text-lg font-bold cursor-pointer">
+                     {categories.map((category) => (
+                        <li
+                           key={category.name}
+                           onClick={() => setSelectedCategory(category.name)}
+                           className={`${
+                              selectedCategory === category.name ? "text-sky-500" : "text-neutral-800"
+                           } hover:text-sky-500`}>
+                           <Link
+                              href={`/explore/places/${category.name === "식당" ? "restaurants" : "accommodations"}`}>
+                              {category.name}
+                           </Link>
+                        </li>
+                     ))}
+                  </ul>
+
+                  {/* 검색 바 */}
+                  <div className="flex">
+                     <div className="relative">
+                        <input
+                           type="text"
+                           placeholder="검색어를 입력해 주세요."
+                           value={searchTerm}
+                           onChange={handleSearchChange}
+                           className="h-[32px] w-72 p-3 pr-10 border border-sky-500 rounded-lg placeholder:text-sm focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500"
+                        />
+                        <svg
+                           aria-hidden="true"
+                           fill="currentColor"
+                           viewBox="0 0 20 20"
+                           className="w-4 h-4 absolute top-1/2 right-3 transform -translate-y-1/2 text-sky-500">
+                           <path d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
+                        </svg>
+                     </div>
+                     <button
+                        className="h-[32px] px-4 text-white bg-sky-500 text-sm font-medium rounded-lg ml-2"
+                        onClick={handleSearchClick}>
+                        검색
+                     </button>
+                  </div>
+               </div>
+            </div>
+         </div>
+
+         {/* 식당 리스트 */}
+         <div className="max-w-[1409px] mx-auto">
+            <div className="max-w-[1409px] mx-auto mb-[200px]">
+               {/* 음식 카테고리 이미지 버튼 */}
+               <div className="flex justify-between items-center mb-[145px]">
+                  {[
+                     {
+                        src: "/images/places/restaurants/bibimbap.png",
+                        alt: "한식",
+                        text: "한식",
+                        onClick: () => alert("한식 버튼 클릭됨"),
+                     },
+                     {
+                        src: "/images/places/restaurants/pizza.png",
+                        alt: "양식",
+                        text: "양식",
+                        onClick: () => alert("양식 버튼 클릭됨"),
+                     },
+                     {
+                        src: "/images/places/restaurants/manchow-soup.png",
+                        alt: "중식",
+                        text: "중식",
+                        onClick: () => alert("중식 버튼 클릭됨"),
+                     },
+                     {
+                        src: "/images/places/restaurants/nigiri.png",
+                        alt: "일식",
+                        text: "일식",
+                        onClick: () => alert("일식 버튼 클릭됨"),
+                     },
+                     {
+                        src: "/images/places/restaurants/hot-drink.png",
+                        alt: "카페/전통찻집",
+                        text: "카페/전통찻집",
+                        onClick: () => alert("카페/전통찻집 버튼 클릭됨"),
+                     },
+                     {
+                        src: "/images/places/restaurants/tacos.png",
+                        alt: "이색음식점",
+                        text: "이색음식점",
+                        onClick: () => alert("이색음식점 버튼 클릭됨"),
+                     },
+                  ].map(({ src, alt, text, onClick }) => (
+                     <div key={alt} className="flex flex-col items-center">
+                        <button
+                           className="w-[200px] h-[200px] rounded-full overflow-hidden focus:outline-none flex items-center justify-center bg-neutral-100 hover:bg-sky-100 hover:scale-105 hover:shadow-md"
+                           onClick={onClick}>
+                           <Image src={src} alt={alt} width={80} height={80} className="object-cover" />
+                           <div className="absolute inset-0 flex items-center justify-center"></div>
+                        </button>
+                        <span className="text-center text-xl font-bold mt-4">{text}</span>
+                     </div>
+                  ))}
+               </div>
+            </div>
+            {/* ///////////////////////////////////////////////////////// */}
+            {/* ///////////////////////////////////////////////////////// */}
+            {/* ///////////////////////////////////////////////////////// */}
+            {/* 카드 컴포넌트 부분 */}
+            <div className="">
+               <div className="flex justify-center items-center mb-44">
+                  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+                     {[...Array(16)].map((_, index) => (
+                        <RestaurantCard
+                           key={index}
+                           imageUrl="/images/places/restaurants/WesternFood.png"
+                           title={`강릉 함박스테이크 ${index + 1}`}
+                           area="양양군"
+                           category="양식"
+                           buttonText="영업중"
+                        />
+                     ))}
+                  </div>
+               </div>
+            </div>
+         </div>
+         <Footer />
+      </div>
+   );
+}
---
+++ src/components/places/restaurantCard.tsx
@@ -0,0 +1,45 @@
+// 식당 및 숙소 - 식당 카드 컴포넌트
+import Image from "next/image";
+
+interface RestaurantCardProps {
+   imageUrl: string;
+   title: string;
+   area: string;
+   category: string;
+   buttonText: string;
+}
+
+const RestaurantCard = ({ imageUrl, title, area, category, buttonText }: RestaurantCardProps) => {
+   return (
+      <div className="relative w-[300px] h-[326px] bg-white rounded-lg overflow-hidden shadow-lg transition-all duration-300 hover:shadow-xl hover:transform hover:translate-y-[-5px] cursor-pointer flex flex-col">
+         {/* 이미지 */}
+         <div className="w-full h-[198px] relative">
+            <Image src={imageUrl} alt={title} layout="fill" objectFit="cover" />
+         </div>
+
+         <div className="px-4 py-2 flex flex-col flex-grow">
+            {/* 지역 텍스트 */}
+            <div className="text-base font-light text-neutral-500">{area}</div>
+            {/* 타이틀 */}
+            <div className="text-2xl font-semibold text-neutral-800">{title}</div>
+            {/* 식당 카테고리 */}
+            <div className="text-xl font-semibold text-neutral-500 mb-2">{category}</div>
+            {/* 영업중 여부 버튼 */}
+            <div className="mt-auto ml-auto">
+               <button
+                  className={`w-[69px] h-[28px] flex justify-center items-center text-sm font-semibold rounded-lg ${
+                     buttonText === "영업중"
+                        ? "bg-amber-100 text-sky-500 hover:bg-amber-50 "
+                        : "bg-sky-100 text-neutral-500 cursor-not-allowed"
+                  }`}
+                  disabled={buttonText !== "영업중"}
+                  onClick={() => alert("영업중 버튼 클릭됨")}>
+                  {buttonText}
+               </button>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default RestaurantCard;
