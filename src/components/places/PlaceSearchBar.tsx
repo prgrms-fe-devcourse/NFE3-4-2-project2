a500a8fa70c050e40d3ee3b7621fecd90cf3792f
jihye choi <jihyec22@gmail.com>
2025-02-04 10:34:37 +0900

    Feat : 기본 Place 서치 바 (검색X) - 검색 기능은 추후 작업 - 식당 / 숙소 URL 파라미터 cat으로 처리 - 지역 코드는 travel 쪽에 맞춰 URL 파라미터 filter로 처리

---
+++ src/components/places/PlaceSearchBar.tsx
@@ -0,0 +1,74 @@
+import { PlaceSelectedChildParam } from "@/types/types";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import regionList from "@/utils/regionList.json";
+
+const PlaceSearchBar: React.FC<PlaceSelectedChildParam> = ({ selected, changeUrl }) => {
+   const nowPath = usePathname();
+   const categories = [
+      { name: "식당", link: "explore/places/restaurants", value: "restaurant" },
+      { name: "숙소", link: "explore/places/accommodations", value: "accommodation" },
+   ];
+
+   return (
+      <div className="mx-auto w-[860]  p-7 shadow-xl bg-white rounded-lg">
+         <div className="flex justify-between">
+            {/* 식당 & 숙소 탭 */}
+            <div className="flex gap-x-6 font-bold text-xl items-start">
+               {categories.map((cat, idx) => (
+                  <Link
+                     key={cat.value + idx}
+                     href={{
+                        pathname: nowPath,
+                        query: {
+                          cat: cat.value,
+                          page:1,
+                          ...(selected.filter ? { filter: selected.filter } : {})
+                        }                        
+                      }}
+                      scroll={false}
+                     className={`hover:text-sky-500 ${selected.cat === cat.value ? "text-sky-500" : ""}`}>
+                     {cat.name}
+                  </Link>
+               ))}
+            </div>
+            {/* 지역 필터 */}
+            <div className="flex">
+               <div className="w-[180] mr-2">
+                  <p className="text-neutral-500 text-lg pb-2">지역</p>
+                  <select
+                     className="w-full bg-transparent focus:outline-none border-b border-sky-500 text-lg pb-2"
+                     value={selected.filter? selected.filter : ""}
+                     onChange={(e) => {
+                        changeUrl({ ...selected, filter: e.target.value });
+                     }}>
+                     <option value="" className="text-neutral-800">
+                        전체
+                     </option>
+                     {regionList.map((region) => (
+                        <option key={region.code} value={region.code} className="text-neutral-800">
+                           {region.name}
+                        </option>
+                     ))}
+                  </select>
+               </div>
+            </div>
+
+            {/* 검색 바 */}
+            <div className="flex items-start">
+               <div className="relative">
+                  <input
+                     type="text"
+                     placeholder="검색어를 입력해 주세요."
+                     className="text-lg placeholder:text-base w-72 px-4 py-1 border border-sky-500 rounded-lg  focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500"
+                  />
+                  <i className="bi bi-search text-sky-500 absolute right-3 top-[4] text-lg"></i>
+               </div>
+               <button className="px-6 py-1 text-white bg-sky-500 text-lg font-medium rounded-lg ml-2 flex-grow"> 검색</button>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default PlaceSearchBar;
