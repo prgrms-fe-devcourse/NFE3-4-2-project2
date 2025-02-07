9e078965bbff33bd29a591072087ccf94d7221d4
jihye choi <jihyec22@gmail.com>
2025-02-07 10:28:01 +0900

    Chore : tailwind.config 수정 - 일반 CSS에서도 tailwind Color를 css 변수로 사용할 수 있게끔 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,35 @@
+import plugin from "tailwindcss/plugin";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/**/*.{js,ts,jsx,tsx}', // 전체 소스 파일 경로 포함
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        tilt:["Tilt Warp", "san-serif"]
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+        plugin(function ({ addBase, theme }) {
+      const colors = theme("colors");
+
+      const variables = Object.keys(colors).reduce((acc, key) => {
+        if (typeof colors[key] === "string") {
+          acc[`--${key}`] = colors[key];
+        } else {
+          Object.keys(colors[key]).forEach((shade) => {
+            acc[`--${key}-${shade}`] = colors[key][shade];
+          });
+        }
+        return acc;
+      }, {});
+
+      addBase({
+        ":root": variables,
+      });
+    }),
+  ],
 };
