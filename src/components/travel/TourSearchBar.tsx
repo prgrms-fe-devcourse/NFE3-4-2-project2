6b2d73e828e8541ceebd0877eacceec22ffd82a0
cj2174 <cj2174@naver.com>
2025-02-08 13:36:36 +0900

    Refactor : 관광지바 리스트 수정

--- src/components/travel/TourSearchBar.tsx
+++ src/components/travel/TourSearchBar.tsx
@@ -25,27 +25,28 @@ const TourSearchBar: React.FC<SelectedChildParam> = ({ selected, changeUrl }) =>
    return (
       <div className="bg-sky-50 w-full flex justify-center items-start p-28 h-[480px]">
          <div className="w-full max-w-screen-xl mx-auto flex flex-col gap-6 px-6">
+            <h2 className="text-gray-800 text-3xl font-bold tracking-wide mt-2">강원도의</h2>
 
-      <h2 className="text-gray-800 text-3xl font-bold tracking-wide mt-2">강원도의</h2>
+            <div className="flex flex-row items-center gap-3">
+               <span className="text-gray-900 text-xl font-semibold">특별한</span>
 
-
-<div className="flex flex-row items-center gap-3">
-   <span className="text-gray-900 text-xl font-semibold">특별한</span>
-
-   {/* 셀렉트 박스 */}
-   <div className="relative">
-      <select
-         className="border-b-2 border-sky-600 text-sky-600 text-3xl font-bold bg-transparent focus:outline-none cursor-pointer w-fit 
-         [&>option]:text-lg hover:text-sky-700 transition">
-         <option value={"season"}>계절별 관광지</option>
-         <option value={"region"}>지역별 관광지</option>
-         <option value={"culture"}>문화·역사별 관광지</option>
-         <option value={"nature"}>자연별 관광지</option>
-      </select>
-   </div>
+               {/* 셀렉트 박스 */}
+               <div className="relative">
+                  <select
+                     className="border-b-2 border-sky-600 text-sky-600 text-3xl font-bold bg-transparent focus:outline-none cursor-pointer w-fit 
+                     [&>option]:text-lg hover:text-sky-700 transition"
+                     value={selected.cat}
+                     onChange={handleSelectChange}>
+                     <option value="season">계절별 관광지</option>
+                     <option value="region">지역별 관광지</option>
+                     <option value="culture">문화·역사별 관광지</option>
+                     <option value="nature">자연별 관광지</option>
+                  </select>
+               </div>
 
                <span className="text-neutral-600 text-2xl font-semibold">찾아보기</span>
             </div>
+            {/* 선택된 카테고리에 따른 리스트 */}
             {selected.cat === "region" && <RegionList selected={selected} changeUrl={changeUrl} />}
             {selected.cat === "season" && <SeasonBar selected={selected} changeUrl={changeUrl} />}
             {selected.cat === "nature" && <NatureBar selected={selected} changeUrl={changeUrl} />}
